92a6aa28e936126a3ad9ae4f8f5c39a6aa002833
Rumi <moonding901212@gmail.com>
2024-01-29 23:15:11 +0900

    회원가입 기능 구현

--- board-app/src/auth/user.repository.ts
+++ board-app/src/auth/user.repository.ts
@@ -1,4 +1,13 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
-export class UserRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}

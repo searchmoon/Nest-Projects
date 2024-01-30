5db5f5783c85c8f5b0c51e28dfb318c98708cd0d
Rumi <moonding901212@gmail.com>
2024-01-30 16:17:57 +0900

    회원가입 완성

--- board-app/src/auth/user.repository.ts
+++ board-app/src/auth/user.repository.ts
@@ -1,13 +1,30 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
 
-    await this.save(user);
+    const salt = await bcrypt.genSalt(); //salt 생성
+    const hashedPassword = await bcrypt.hash(password, salt); //비밀번호 해시하기
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }

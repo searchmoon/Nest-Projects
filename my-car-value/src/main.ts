94fe67d53d0935b052adcee6eb551fb61e123104
Rumi <moonding901212@gmail.com>
2024-02-05 18:19:23 +0900

    인증(데코레이터, 인터셉트, 가드) 완료

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['asdfasfd'],
+    }),
+  );
+
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 
 @Controller('auth')
@@ -15,15 +18,29 @@ export class UsersController {
     private authService: AuthService
   ) {}
 
-  @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
-    // this.usersService.create(body.email, body.password);
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
-  
+
+  @Post('/signout')
+  signOut(@Session() session: any){
+    session.userId = null;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')

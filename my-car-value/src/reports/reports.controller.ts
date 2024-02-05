c777bf20f0fd98ccdc223b0405606da753e5a7a2
Rumi <moonding901212@gmail.com>
2024-02-06 01:43:05 +0900

    typeORM을 이용한 관계 설정

--- my-car-value/src/reports/reports.controller.ts
+++ my-car-value/src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CreateReportDto } from './dtos/create-report-dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  // @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}

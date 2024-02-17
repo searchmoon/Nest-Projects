837bd187df492c6f28594e4da7d0db0a2ecf5fc4
Rumi <moonding901212@gmail.com>
2024-02-17 23:31:19 +0900

    typeorm 설정 추가

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -7,6 +7,15 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -15,6 +24,30 @@ import { DmsModule } from './dms/dms.module';
     WorkspacesModule,
     ChannelsModule,
     DmsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME || 'rumi',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      migrations: [__dirname + '/migrations/*.ts'],
+      charset: 'utf8mb4_general_ci',
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],

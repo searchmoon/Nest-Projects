5922cfe4fd7720f4104263c67ebf63714fb05614
Rumi <moonding901212@gmail.com>
2024-02-20 01:14:26 +0900

    트랜잭션 추가

--- a-nest/src/workspaces/workspaces.service.ts
+++ a-nest/src/workspaces/workspaces.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { ChannelMembers } from '../entities/ChannelMembers';
 import { Channels } from '../entities/Channels';
 import { Users } from '../entities/Users';
@@ -9,16 +9,19 @@ import { Workspaces } from '../entities/Workspaces';
 
 @Injectable()
 export class WorkspacesService {
-  @InjectRepository(Workspaces)
-  private workspacesRepository: Repository<Workspaces>;
-  @InjectRepository(Channels)
-  private channelsRepository: Repository<Channels>;
-  @InjectRepository(WorkspaceMembers)
-  private workspaceMembersRepository: Repository<WorkspaceMembers>;
-  @InjectRepository(ChannelMembers)
-  private channelMembersRepository: Repository<ChannelMembers>;
-  @InjectRepository(Users)
-  private usersRepository: Repository<Users>;
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+    private dataSource: DataSource,
+  ) {}
 
   async findById(id: number) {
     return this.workspacesRepository.findOne({ where: { id } });
@@ -33,23 +36,45 @@ export class WorkspacesService {
   }
 
   async createWorkspace(name: string, url: string, myId: number) {
-    const workspace = new Workspaces();
-    workspace.name = name;
-    workspace.url = url;
-    workspace.OwnerId = myId;
-    const returned = await this.workspacesRepository.save(workspace);
-    const workspaceMember = new WorkspaceMembers();
-    workspaceMember.UserId = myId;
-    workspaceMember.WorkspaceId = returned.id;
-    await this.workspaceMembersRepository.save(workspaceMember);
-    const channel = new Channels();
-    channel.name = '일반';
-    channel.WorkspaceId = returned.id;
-    const channelReturned = await this.channelsRepository.save(channel);
-    const channelMember = new ChannelMembers();
-    channelMember.UserId = myId;
-    channelMember.ChannelId = channelReturned.id;
-    await this.channelMembersRepository.save(channelMember);
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const returned = await queryRunner.manager
+        .getRepository(Workspaces)
+        .save({
+          name,
+          url,
+          OwnerId: myId,
+        });
+
+      await queryRunner.manager.getRepository(WorkspaceMembers).save({
+        UserId: myId,
+        WorkspaceId: returned.id,
+      });
+
+      const channelReturned = await queryRunner.manager
+        .getRepository(Channels)
+        .save({
+          name: '일반',
+          WorkspaceId: returned.id,
+        });
+
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: myId,
+        ChannelId: channelReturned.id,
+      });
+
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async getWorkspaceMembers(url: string) {
@@ -73,8 +98,10 @@ export class WorkspacesService {
       },
     });
     const user = await this.usersRepository.findOne({ where: { email } });
+
     if (!user) {
       return null;
+      // return new NotFoundException('사용자가 없습니다.');
     }
     const workspaceMember = new WorkspaceMembers();
     workspaceMember.WorkspaceId = workspace.id;

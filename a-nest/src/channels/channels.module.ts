b043396874647d6e66ebc609876fe99a3c7ebd6e
Rumi <moonding901212@gmail.com>
2024-02-20 11:13:49 +0900

    channels controller, service 기능 붙이기

--- a-nest/src/channels/channels.module.ts
+++ a-nest/src/channels/channels.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { ChannelChats } from 'src/entities/ChannelChats';
+import { Workspaces } from 'src/entities/Workspaces';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Users,
+      ChannelMembers,
+      Channels,
+      ChannelChats,
+      Workspaces,
+    ]),
+  ],
   controllers: [ChannelsController],
   providers: [ChannelsService],
 })

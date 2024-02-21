a2537d33ea660646a41a1a52e09a70504e20f46f
Rumi <moonding901212@gmail.com>
2024-02-21 14:22:57 +0900

    swagger 설정

--- study-room/src/main.ts
+++ study-room/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Study Room Booking')
+    .setDescription('The Study Room API 문서입니다.')
+    .setVersion('1.0')
+    .addTag('study-room-api')
+    // .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
